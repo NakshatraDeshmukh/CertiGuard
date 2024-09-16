00e1ff5395eb3f8cac8ddf05d7a114d4a42a0899
NakshatraDeshmukh <nakshatrad5075@gmail.com>
2024-09-17 00:29:08 +0530

    Minor Updates

--- app.py
+++ app.py
@@ -302,10 +302,7 @@ def get_university(university_id):
         return jsonify({'error': str(e)}), 500
 
 
+if __name__ == '__main__': 
+    app.run(debug=True) 
 
 
-
-
-
-if __name__ == '__main__':
-    app.run()
